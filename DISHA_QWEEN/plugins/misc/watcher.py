0674f1581ad867a5dcfdd8c0255977aa1ac8ab5b
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-07-20 12:37:34 +0530

    Update watcher.py

--- DISHA_QWEEN/plugins/misc/watcher.py
+++ DISHA_QWEEN/plugins/misc/watcher.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from PURVIMUSIC import app
-from PURVIMUSIC.core.call import PURVI
+from DISHA_QWEEN import app
+from DISHA_QWEEN.core.call import PURVI
 
 welcome = 20
 close = 30
@@ -11,4 +11,4 @@ close = 30
 @app.on_message(filters.video_chat_started, group=welcome)
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
-    await PURVI.stop_stream_force(message.chat.id)
+    await DISHA.stop_stream_force(message.chat.id)
