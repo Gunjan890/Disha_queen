22e0e70d26fac3dc40154ce740661fec5e81f1a6
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-07-20 12:59:41 +0530

    Update ping.py

--- DISHA_QWEEN/plugins/tools/ping.py
+++ DISHA_QWEEN/plugins/tools/ping.py
@@ -4,11 +4,11 @@ from pyrogram import filters
 from pyrogram.types import Message
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
 from config import *
-from PURVIMUSIC import app
-from PURVIMUSIC.core.call import PURVI
-from PURVIMUSIC.utils import bot_sys_stats
-from PURVIMUSIC.utils.decorators.language import language
-from PURVIMUSIC.utils.inline import supp_markup
+from DISHA_QWEEN import app
+from DISHA_QWEEN.core.call import PURVI
+from DISHA_QWEEN.utils import bot_sys_stats
+from DISHA_QWEEN.utils.decorators.language import language
+from DISHA_QWEEN.utils.inline import supp_markup
 from config import BANNED_USERS
 
 
@@ -17,10 +17,10 @@ from config import BANNED_USERS
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_video(
-        video="https://graph.org/file/5690109178f081adf464d.mp4",
+        video="https://graph.org/file/94918b340445db8a72c02.jpg",
         caption=_["ping_1"].format(app.mention),
     )
-    pytgping = await PURVI.ping()
+    pytgping = await DISHA.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
