84b3d7dc76e1e312d7900fd48bcf6426fb454e63
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-07-20 12:46:23 +0530

    Update maintenance.py

--- DISHA_QWEEN/plugins/sudo/maintenance.py
+++ DISHA_QWEEN/plugins/sudo/maintenance.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from PURVIMUSIC import app
-from PURVIMUSIC.misc import SUDOERS
-from PURVIMUSIC.utils.database import (
+from DISHA_QWEEN import app
+from DISHA_QWEEN.misc import SUDOERS
+from DISHA_QWEEN.utils.database import (
     get_lang,
     is_maintenance,
     maintenance_off,
