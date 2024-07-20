10f5724ad6f547c4cf492b5b4668be22b7aaefb8
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-07-20 15:44:09 +0530

    Update Telegram.py

--- DISHA_QWEEN/platforms/Telegram.py
+++ DISHA_QWEEN/platforms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from PURVIMUSIC import app
-from PURVIMUSIC.utils.formatters import (
+from DISHA_QWEEN import app
+from DISHA_QWEEN.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
