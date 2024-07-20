50e6abf8fb4d8afbfb732b8a4a6c646311f36159
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-07-20 13:52:38 +0530

    Update channelplay.py

--- DISHA_QWEEN/utils/channelplay.py
+++ DISHA_QWEEN/utils/channelplay.py
@@ -1,5 +1,5 @@
-from PURVIMUSIC import app
-from PURVIMUSIC.utils.database import get_cmode
+from DISHA_QWEEN import app
+from DISHA_QWEEN.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
