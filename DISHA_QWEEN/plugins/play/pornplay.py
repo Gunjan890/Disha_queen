381207857e37cdd0bf552e1f57e5bf5067a2fc16
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-07-20 12:41:27 +0530

    Update pornplay.py

--- DISHA_QWEEN/plugins/play/pornplay.py
+++ DISHA_QWEEN/plugins/play/pornplay.py
@@ -1,13 +1,13 @@
 from pyrogram import filters
 import requests, random
 from bs4 import BeautifulSoup
-from PURVIMUSIC import app
+from DISHA_QWEEN import app
 import pytgcalls
 import os, yt_dlp 
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pytgcalls.types import AudioVideoPiped
-from PURVIMUSIC.plugins.play import play
-from PURVIMUSIC.plugins.play.pornplay import play
+from DISHA_QWEEN.plugins.play import play
+from DISHA_QWEEN.plugins.play.pornplay import play
 
 #
 #####
