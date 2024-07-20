b3137451907f03b24d4cdad422d7dd1b06d27525
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-07-20 14:00:00 +0530

    Update sys.py

--- DISHA_QWEEN/utils/sys.py
+++ DISHA_QWEEN/utils/sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from PURVIMUSIC.misc import _boot_
-from PURVIMUSIC.utils.formatters import get_readable_time
+from DISHA_QWEEN.misc import _boot_
+from DISHA_QWEEN.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
