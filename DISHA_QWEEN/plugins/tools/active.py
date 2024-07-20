28fd679e50c830e589a0a6035a85d4f9049eed60
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-07-20 12:55:10 +0530

    Update active.py

--- DISHA_QWEEN/plugins/tools/active.py
+++ DISHA_QWEEN/plugins/tools/active.py
@@ -2,9 +2,9 @@ from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from unidecode import unidecode
 
-from PURVIMUSIC import app
-from PURVIMUSIC.misc import SUDOERS
-from PURVIMUSIC.utils.database import (
+from DISHA_QWEEN import app
+from DISHA_QWEEN.misc import SUDOERS
+from DISHA_QWEEN.utils.database import (
     get_active_chats,
     get_active_video_chats,
     remove_active_chat,
